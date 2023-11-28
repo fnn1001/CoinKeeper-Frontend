7e8fbd4a6d717795bf18447636c1a78f1582de1e
John Doe <johndoe@example.com>
2023-11-28 20:05:28 +0100

    added side bar logout button

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,49 +1,81 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Invest from '../../pages/Investment/Invest';
+// Import necessary components and styles
+import React, { useState, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Sidebar.css';
+import { AuthContext } from '../../context/auth.context';
 
-const Sidebar = ({ isLoggedIn, logOutUser, user }) => {
+// Sidebar component
+const Sidebar = (props) => {
+  // State for managing sidebar visibility
   const [sidebarVisible, setSidebarVisible] = useState(false);
 
+  const { isLoggedIn, logOutUser, user } = useContext(AuthContext);
+
+  // React Router navigate for programmatic navigation
+  const navigate = useNavigate();
+
+  // Function to toggle sidebar visibility
   const toggleSidebar = () => {
     setSidebarVisible(!sidebarVisible);
   };
 
+  // Function to handle link click based on authentication status
+  const handleLinkClick = (path) => {
+    toggleSidebar();
+    
+    // Redirect to signup if not logged in (except for the Login/Sign Up link)
+    if (!isLoggedIn && path !== '/signup') {
+      navigate('/signup');
+    }
+  };
+
   return (
     <>
+      {/* Sidebar container */}
       <div className={`sidebar ${sidebarVisible ? 'open' : ''}`}>
-        <Link to="/" className="sidebar-link" onClick={toggleSidebar}>
+        {/* Navigation links */}
+        <Link to="/" className="sidebar-link" onClick={() => handleLinkClick('/')}>
           Home
         </Link>
-        <Link to="/budgets" className="sidebar-link" onClick={toggleSidebar}>
+        <Link to="/budgets" className="sidebar-link" onClick={() => handleLinkClick('/budgets')}>
           My budgets
         </Link>
 
-        {/* Add more navigation links as needed */}
+        {/* Additional links for logged-in users */}
         {isLoggedIn && (
           <>
-            <Link to="/profile" className="sidebar-link" onClick={toggleSidebar}>
+            <Link to="/profile" className="sidebar-link" onClick={() => handleLinkClick('/profile')}>
               Profile
             </Link>
-            <button onClick={logOutUser} className="sidebar-link">
-              Logout
-            </button>
-            <span>{user && user.name}</span>
+            <div className="user-info">
+              <span>{user && user.name}</span>
+              <button onClick={logOutUser} className="logout-btn">
+                Logout
+              </button>
+            </div>
           </>
         )}
-            <Link to="/invest" className="sidebar-link" onClick={toggleSidebar}>
-              My Investments
-            </Link>
 
-        {!isLoggedIn && (
-          <Link to="/signup" className="sidebar-link" onClick={toggleSidebar}>
-            Login/Sign Up
+        {/* Investment link */}
+        <Link to="/invest" className="sidebar-link" onClick={() => handleLinkClick('/invest')}>
+          My Investments
+        </Link>
+
+        {/* Login/Sign Up or Logout link based on authentication status */}
+        {!isLoggedIn ? (
+          <Link to="/signup" className="sidebar-link">
+            <span onClick={() => handleLinkClick('/signup')}>Login/Sign Up</span>
           </Link>
+        ) : (
+          <button onClick={logOutUser} className="sidebar-link">
+            Logout
+          </button>
         )}
       </div>
 
+      {/* Main content container with dynamic margin based on sidebar visibility */}
       <div id="main" style={{ marginLeft: sidebarVisible ? '250px' : '0' }}>
+        {/* Sidebar toggle button */}
         <div className="sidebar-navbar">
           <button className={`menu-btn ${sidebarVisible ? 'open' : ''}`} onClick={toggleSidebar}>
             <span></span>
@@ -56,4 +88,5 @@ const Sidebar = ({ isLoggedIn, logOutUser, user }) => {
   );
 };
 
+// Export the Sidebar component
 export default Sidebar;
