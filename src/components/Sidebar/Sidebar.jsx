c8eb6d3c6ec4cdc06b956347c0acc4c21762a998
Lea Mng <lea.monsenego@gmail.com>
2023-12-02 14:52:03 +0100

    import fixed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 // DEPENDENCIES
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 //STYLES
 import './Sidebar.css';
